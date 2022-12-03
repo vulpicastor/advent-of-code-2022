4f4acd20fdd154952fa627fdf88cd3faeeb00ff7
Lizhou Sha <lizhou@lsha.net>
2022-12-02 23:58:26 -0600

    new.py: Solution template

---
+++ src/new.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# pylint: disable=unused-import
+import collections
+import functools
+import io
+import itertools
+import operator as op
+import re
+import timeit
+
+import numpy as np
+import aocd
+
+YEAR = 2021
+DAY = 1
+
+
+
+
+
+def main():
+    data = aocd.get_data(day=DAY, year=YEAR)
+    inlist = data.split('\n')
+
+    # aocd.submit(answer, part='a', day=DAY, year=YEAR)
+
+    # aocd.submit(answer, part='b', day=DAY, year=YEAR)
+
+
+if __name__ == '__main__':
+    main()
