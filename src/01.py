9247c3acea84e3a0cbc0bb3d346436530b214783
Lizhou Sha <lizhou@lsha.net>
2022-12-03 00:16:51 -0600

    Day 01: Cleanup

--- src/01.py
+++ src/01.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=unspecified-encoding
 # pylint: disable=unused-import
-import collections
-import functools
-import io
-import itertools
-import operator as op
-import re
-import timeit
-
-import numpy as np
 import aocd
 
-YEAR = 2021
+YEAR = 2022
 DAY = 1
 
 
