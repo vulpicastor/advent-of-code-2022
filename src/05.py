0b11a89016a4f99e380646144cd1ce35b52ab6b8
Lizhou Sha <lizhou@lsha.net>
2022-12-04 23:55:33 -0600

    Day 05: Cleanup

--- src/05.py
+++ src/05.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
-# pylint: disable=unused-import
-import collections
-import functools
-import io
-import itertools
-import operator as op
+import copy
 import re
-import timeit
 
 import numpy as np
 import aocd
@@ -18,8 +12,8 @@ DAY = 5
 
 def parse_move(s):
     m = re.match(r'move (\d+) from (\d+) to (\d+)', s)
-    # print(m.group(1, 2 , 3))
     return int(m.group(1)), int(m.group(2)), int(m.group(3))
+
 
 def do_move(stacks, move):
     n, fr, to = move
@@ -27,21 +21,11 @@ def do_move(stacks, move):
         stacks[to].append(stacks[fr].pop())
     return stacks
 
+
 def do_move2(stacks, move):
     n, fr, to = move
-
-    print(move)
-    if len(stacks[fr]) < n:
-        moved = stacks[fr][-n:]
-        print(moved)
-        raise ValueError(f'Insufficient crates on stack {fr}')
-    moved = stacks[fr][-n:]
-    stacks[to].extend(moved)
-    stacks[fr] = stacks[fr][:-n]
-    # for l in stacks[1:]:
-        # print(''.join(l))
-    print(fr, ''.join(stacks[fr]))
-    print(to, ''.join(stacks[to]))
+    stacks[to].extend(stacks[fr][-n:])
+    del stacks[fr][-n:]
     
 
 def main():
@@ -58,27 +42,24 @@ move 1 from 1 to 2"""
     dock, moves = data.split('\n\n')
     moves = moves.split('\n')
     docka = np.array([list(l) for l in dock.split('\n')])
+    # Horrible Numpy hack to transpose the input.
     stacks = [[c for c in l if c != ' '] for l in (docka[::-1, :].T)[1::4, 1:]]
+    # I don't want to think about 1-indexing.
     stacks.insert(0, [])
-    # print(stacks)
-    # print(moves[:10])
-    # for _, m in zip(range(2), moves):
-    for l in stacks[1:]:
-        print(''.join(l))
+    # Save a copy for part 2.
+    stacks2 = copy.deepcopy(stacks)
+
     for m in moves:
-        # if m:
-            do_move2(stacks, parse_move(m))
-    # inlist = [l for l in data.split('\n')]
-    print(stacks)
-
-    # answer = ''.join(s[-1] for s in stacks if s)
-    # print(answer)
-    # aocd.submit(answer, part='a', day=DAY, year=YEAR)
-
-    answer = ''.join(s[-1] if s else ' ' for s in stacks[1:])
+        do_move(stacks, parse_move(m))
+    answer = ''.join(s[-1] for s in stacks if s)
     print(answer)
-    # answer = 
-    # print(answer)
+    aocd.submit(answer, part='a', day=DAY, year=YEAR)
+
+    stacks = stacks2
+    for m in moves:
+        do_move2(stacks, parse_move(m))
+    answer = ''.join(s[-1] for s in stacks if s)
+    print(answer)
     aocd.submit(answer, part='b', day=DAY, year=YEAR)
 
 
